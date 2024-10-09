3b0bac411696649898a7ebd13db30e2d4f271143
SpreadSheets600 <Sohammaity006@gmail.com>
2024-10-09 21:10:26 +0530

    Added Tracked GitHub Repos

--- COGS/GitHub.py
+++ COGS/GitHub.py
@@ -13,7 +13,6 @@ def iso_unix_timestamp(iso: str):
         return uix
     except ValueError as e:
         print(f"Error parsing date: {e}")
-        return 0
 
 
 class GitHub(commands.Cog):
@@ -43,21 +42,25 @@ class GitHub(commands.Cog):
 
         embed = discord.Embed(
             title="YARS Repository Information",
-            description=data.get("description", "No description available."),
+            description=f"{data.get("description", "No description available.")}\n### Language : {data['language']}\n",
+            color=discord.Color.blue(),
+            url=data.get("html_url", ""),
         )
 
-        embed.add_field(name="🍴 Forks", value=data.get("forks_count", 0), inline=False)
+        embed.set_author(
+            name=f"Created By : {data.get("owner", {}).get("login", "Unknown")}",
+            icon_url=data.get("owner", {}).get("avatar_url", ""),
+        )
+
+        embed.add_field(name="🍴 Forks", value=data.get("forks_count", 0), inline=True)
         embed.add_field(
-            name="⭐ Stars", value=data.get("stargazers_count", 0), inline=False
+            name="⭐ Stars", value=data.get("stargazers_count", 0), inline=True
         )
         embed.add_field(
-            name="👀 Watchers", value=data.get("watchers_count", 0), inline=False
+            name="👀 Watchers", value=data.get("watchers_count", 0), inline=True
         )
         embed.add_field(
-            name="🏮 Language", value=data.get("language", "N/A"), inline=False
-        )
-        embed.add_field(
-            name="📝 Open Issues", value=data.get("open_issues_count", 0), inline=False
+            name="📝 Open Issues", value=data.get("open_issues_count", 0), inline=True
         )
 
         created_at = data.get("created_at", None)
@@ -67,23 +70,235 @@ class GitHub(commands.Cog):
             embed.add_field(
                 name="🕒 Created At",
                 value=f"<t:{iso_unix_timestamp(created_at)}:R>",
-                inline=False,
+                inline=True,
             )
         else:
-            embed.add_field(name="🕒 Created At", value="Unknown", inline=False)
+            embed.add_field(name="🕒 Created At", value="Unknown", inline=True)
 
         if updated_at:
             embed.add_field(
                 name="🕒 Updated At",
                 value=f"<t:{iso_unix_timestamp(updated_at)}:R>",
-                inline=False,
+                inline=True,
             )
         else:
-            embed.add_field(name="🕒 Updated At", value="Unknown", inline=False)
+            embed.add_field(name="🕒 Updated At", value="Unknown", inline=True)
 
         topics = data.get("topics", [])
         if topics:
-            embed.add_field(name="🔗 Topics", value=" ".join(topics), inline=False)
+            embed.add_field(name="🔗 Topics", value=", ".join(topics), inline=False)
+        else:
+            embed.add_field(
+                name="🔗 Topics", value="No topics available.", inline=False
+            )
+
+        await ctx.followup.send(embed=embed)
+
+    @info.command(name="amine", description="Get Information About AMINE")
+    async def yars(self, ctx):
+        await ctx.defer()
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.github.com/repos/datavorous/amine"
+            ) as response:
+                if response.status != 200:
+                    await ctx.followup.send(
+                        f"Error fetching repository data: {response.status}"
+                    )
+                    return
+
+                data = await response.json()
+
+        embed = discord.Embed(
+            title="YARS Repository Information",
+            description=f"{data.get("description", "No description available.")}\n### Language : {data['language']}\n",
+            color=discord.Color.blue(),
+            url=data.get("html_url", ""),
+        )
+
+        embed.set_author(
+            name=f"Created By : {data.get("owner", {}).get("login", "Unknown")}",
+            icon_url=data.get("owner", {}).get("avatar_url", ""),
+        )
+
+        embed.add_field(name="🍴 Forks", value=data.get("forks_count", 0), inline=True)
+        embed.add_field(
+            name="⭐ Stars", value=data.get("stargazers_count", 0), inline=True
+        )
+        embed.add_field(
+            name="👀 Watchers", value=data.get("watchers_count", 0), inline=True
+        )
+        embed.add_field(
+            name="📝 Open Issues", value=data.get("open_issues_count", 0), inline=True
+        )
+
+        created_at = data.get("created_at", None)
+        updated_at = data.get("updated_at", None)
+
+        if created_at:
+            embed.add_field(
+                name="🕒 Created At",
+                value=f"<t:{iso_unix_timestamp(created_at)}:R>",
+                inline=True,
+            )
+        else:
+            embed.add_field(name="🕒 Created At", value="Unknown", inline=True)
+
+        if updated_at:
+            embed.add_field(
+                name="🕒 Updated At",
+                value=f"<t:{iso_unix_timestamp(updated_at)}:R>",
+                inline=True,
+            )
+        else:
+            embed.add_field(name="🕒 Updated At", value="Unknown", inline=True)
+
+        topics = data.get("topics", [])
+        if topics:
+            embed.add_field(name="🔗 Topics", value=", ".join(topics), inline=False)
+        else:
+            embed.add_field(
+                name="🔗 Topics", value="No topics available.", inline=False
+            )
+
+        await ctx.followup.send(embed=embed)
+
+    @info.command(name="doubtnutx", description="Get Information About DoubtnutX")
+    async def yars(self, ctx):
+        await ctx.defer()
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.github.com/repos/datavorous/DoubtnutPremiumPass"
+            ) as response:
+                if response.status != 200:
+                    await ctx.followup.send(
+                        f"Error fetching repository data: {response.status}"
+                    )
+                    return
+
+                data = await response.json()
+
+        embed = discord.Embed(
+            title="YARS Repository Information",
+            description=f"{data.get("description", "No description available.")}\n### Language : {data['language']}\n",
+            color=discord.Color.blue(),
+            url=data.get("html_url", ""),
+        )
+
+        embed.set_author(
+            name=f"Created By : {data.get("owner", {}).get("login", "Unknown")}",
+            icon_url=data.get("owner", {}).get("avatar_url", ""),
+        )
+
+        embed.add_field(name="🍴 Forks", value=data.get("forks_count", 0), inline=True)
+        embed.add_field(
+            name="⭐ Stars", value=data.get("stargazers_count", 0), inline=True
+        )
+        embed.add_field(
+            name="👀 Watchers", value=data.get("watchers_count", 0), inline=True
+        )
+        embed.add_field(
+            name="📝 Open Issues", value=data.get("open_issues_count", 0), inline=True
+        )
+
+        created_at = data.get("created_at", None)
+        updated_at = data.get("updated_at", None)
+
+        if created_at:
+            embed.add_field(
+                name="🕒 Created At",
+                value=f"<t:{iso_unix_timestamp(created_at)}:R>",
+                inline=True,
+            )
+        else:
+            embed.add_field(name="🕒 Created At", value="Unknown", inline=True)
+
+        if updated_at:
+            embed.add_field(
+                name="🕒 Updated At",
+                value=f"<t:{iso_unix_timestamp(updated_at)}:R>",
+                inline=True,
+            )
+        else:
+            embed.add_field(name="🕒 Updated At", value="Unknown", inline=True)
+
+        topics = data.get("topics", [])
+        if topics:
+            embed.add_field(name="🔗 Topics", value=", ".join(topics), inline=False)
+        else:
+            embed.add_field(
+                name="🔗 Topics", value="No topics available.", inline=False
+            )
+
+        await ctx.followup.send(embed=embed)
+
+    @info.command(
+        name="shouldhavedone", description="Get Information About ShouldHaveDone ?"
+    )
+    async def yars(self, ctx):
+        await ctx.defer()
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.github.com/repos/SpreadSheets600/ShouldHaveDoneIt-BOT"
+            ) as response:
+                if response.status != 200:
+                    await ctx.followup.send(
+                        f"Error fetching repository data: {response.status}"
+                    )
+                    return
+
+                data = await response.json()
+
+        embed = discord.Embed(
+            title="YARS Repository Information",
+            description=f"{data.get("description", "No description available.")}\n### Language : {data['language']}\n",
+            color=discord.Color.blue(),
+            url=data.get("html_url", ""),
+        )
+
+        embed.set_author(
+            name=f"Created By : {data.get("owner", {}).get("login", "Unknown")}",
+            icon_url=data.get("owner", {}).get("avatar_url", ""),
+        )
+
+        embed.add_field(name="🍴 Forks", value=data.get("forks_count", 0), inline=True)
+        embed.add_field(
+            name="⭐ Stars", value=data.get("stargazers_count", 0), inline=True
+        )
+        embed.add_field(
+            name="👀 Watchers", value=data.get("watchers_count", 0), inline=True
+        )
+        embed.add_field(
+            name="📝 Open Issues", value=data.get("open_issues_count", 0), inline=True
+        )
+
+        created_at = data.get("created_at", None)
+        updated_at = data.get("updated_at", None)
+
+        if created_at:
+            embed.add_field(
+                name="🕒 Created At",
+                value=f"<t:{iso_unix_timestamp(created_at)}:R>",
+                inline=True,
+            )
+        else:
+            embed.add_field(name="🕒 Created At", value="Unknown", inline=True)
+
+        if updated_at:
+            embed.add_field(
+                name="🕒 Updated At",
+                value=f"<t:{iso_unix_timestamp(updated_at)}:R>",
+                inline=True,
+            )
+        else:
+            embed.add_field(name="🕒 Updated At", value="Unknown", inline=True)
+
+        topics = data.get("topics", [])
+        if topics:
+            embed.add_field(name="🔗 Topics", value=", ".join(topics), inline=False)
         else:
             embed.add_field(
                 name="🔗 Topics", value="No topics available.", inline=False
